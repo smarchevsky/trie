b6434ae78f7f09c3cb90bfc18402c4f3222b252b
smarchevsky <staseg1987@gmail.com>
2026-02-17 06:09:00 +0200

    Dense trie attempt

--- main.cpp
+++ main.cpp
@@ -1,144 +1,199 @@
 #include <algorithm> // std::lower_bound
+#include <cassert>
 #include <cstring> // memmove
 #include <stdint.h>
 #include <stdio.h>
 #include <utility> // std::pair
 
-// clang-format off
-enum Op : int8_t { opUnknown = 0, parOpen, parClose, opPlus, opMinus, opMul, opDiv, opNegate, opSin, opCos, opTan, opCat, opCar };
-// clang-format on
+template <typename T, typename T2>
+inline constexpr size_t align(T2 unaligned)
+{
+    constexpr size_t alignMask = (alignof(T) - 1);
+    static_assert((alignMask & alignof(T)) == 0); // is pow of 2
+    return (((size_t)unaligned + alignMask) & ~alignMask);
+}
 
-template <typename Key, typename Val>
-class BinarySearchMap {
-    Key* keys = nullptr;
-    Val* vals = nullptr;
-    uint32_t size = 0;
-    uint32_t capacity = 0;
+class TrieNode;
+
+using NumType = uint8_t;
+using KeyType = uint8_t;
+
+class TrieNode {
+    uint8_t* data;
 
 public:
+    bool valid() const { return data; }
+
+    inline void getNumKeyNodes(NumType& outNum, KeyType*& outKeys, TrieNode*& outNodes) const
+    {
+        outNum = data ? *(NumType*)data : 0;
+        outKeys = (KeyType*)align<KeyType>((NumType*)data + 1);
+        outNodes = (TrieNode*)align<TrieNode>(outKeys + outNum);
+    }
+
+    static inline TrieNode* getNodeStartForNewNum(void* dataFrom, NumType newNum)
+    {
+        auto keys = (KeyType*)align<KeyType>((NumType*)dataFrom + 1);
+        return (TrieNode*)align<TrieNode>(keys + newNum);
+    }
+
     struct SoAIterator {
-        const Key* p1;
-        const Val* p2;
+        KeyType* p1;
+        TrieNode* p2;
 
-        std::pair<const Key&, const Val&> operator*() const { return { *p1, *p2 }; }
-
+        std::pair<KeyType&, TrieNode&> operator*() const { return { *p1, *p2 }; }
+        bool operator!=(const SoAIterator& other) const { return p1 != other.p1; }
         SoAIterator& operator++()
         {
             ++p1, ++p2;
             return *this;
         }
-
-        bool operator!=(const SoAIterator& other) const { return p1 != other.p1; }
     };
 
-    auto begin() const { return SoAIterator { keys, vals }; }
-    auto end() const { return SoAIterator { keys + size, vals + size }; }
-
-    Val& insert(const Key& key)
+    auto begin() const
     {
-        auto keyIt = std::lower_bound(keys, keys + size, key);
-        int newKeyPos = std::distance(keys, keyIt);
+        NumType num;
+        KeyType* keys;
+        TrieNode* nodes;
+        getNumKeyNodes(num, keys, nodes);
+        return SoAIterator { keys, nodes };
+    }
 
-        if (keyIt == keys + size || *keyIt != key) {
-            size_t _pos = newKeyPos;
-            if (size >= capacity) {
-                size_t _new_cap = (capacity == 0) ? 4 : capacity * 2;
-                keys = (Key*)realloc(keys, _new_cap * sizeof(*keys));
-                vals = (Val*)realloc(vals, _new_cap * sizeof(*vals));
-                capacity = _new_cap;
+    auto end() const
+    {
+        NumType num;
+        KeyType* keys;
+        TrieNode* nodes;
+        getNumKeyNodes(num, keys, nodes);
+        return SoAIterator { keys + num, nodes + num };
+    }
+
+    TrieNode find(const KeyType& key) const
+    {
+        NumType num;
+        KeyType* keys;
+        TrieNode* nodes;
+        getNumKeyNodes(num, keys, nodes);
+
+        if (num == 0)
+            return {};
+
+        auto keyIt = std::lower_bound(keys, keys + num, key);
+        if (keyIt == keys + num || *keyIt != key)
+            return *(nodes + std::distance(keys, keyIt));
+
+        return {};
+    }
+
+    TrieNode& insert(const KeyType& key)
+    {
+        NumType num;
+        KeyType* keyStart;
+        TrieNode* nodeStart;
+        getNumKeyNodes(num, keyStart, nodeStart);
+
+        auto keyIt = std::lower_bound(keyStart, keyStart + num, key);
+        size_t _pos = std::distance(keyStart, keyIt);
+
+        if (keyIt == keyStart + num || *keyIt != key) {
+            TrieNode* nodeStartNew = getNodeStartForNewNum(data, num + 1);
+            TrieNode* nodeEndNew = nodeStartNew + num + 1;
+
+            uint8_t* dataOld = data;
+            data = (uint8_t*)realloc(dataOld, (size_t)nodeEndNew - (size_t)data);
+
+            if (dataOld == nullptr) {
+                memset(data, 0, (size_t)nodeEndNew);
+                printf("Initial allocation\n");
             }
 
-            if (_pos < size) {
-                memmove(&keys[_pos + 1], &keys[_pos], (size - _pos) * sizeof(*keys));
-                memmove(&vals[_pos + 1], &vals[_pos], (size - _pos) * sizeof(*vals));
+            std::ptrdiff_t dataOffset = data - dataOld;
+            (uint8_t*&)keyStart += dataOffset;
+            (uint8_t*&)nodeStart += dataOffset;
+            (uint8_t*&)nodeStartNew += dataOffset;
+            (uint8_t*&)nodeEndNew += dataOffset;
+
+            if (nodeStartNew != nodeStart) {
+                memmove(nodeStartNew, nodeStart, num * sizeof(*nodeStart));
+                // *nodes = {};
             }
 
-            keys[_pos] = key;
-            vals[_pos] = Val {};
-            size++;
+            if (_pos < num) {
+                memmove(&keyStart[_pos + 1], &keyStart[_pos], (num - _pos) * sizeof(*keyStart));
+                memmove(&nodeStart[_pos + 1], &nodeStart[_pos], (num - _pos) * sizeof(*nodeStart));
+            }
+
+            keyStart[_pos] = key;
+            nodeStart[_pos] = TrieNode {};
+            *(NumType*)data += 1; // increment size
         }
-        return vals[newKeyPos];
+        return nodeStart[_pos];
     }
-
-    const Val* find(const Key& key) const
-    {
-        auto keyIt = std::lower_bound(keys, keys + size, key);
-        if (keyIt == keys + size || *keyIt != key)
-            return &*(vals + std::distance(keys, keyIt));
-        return nullptr;
-    }
-};
-
-struct TrieNode {
-    Op op = opUnknown;
-    BinarySearchMap<char, TrieNode*> children;
 
     void print(int offset) const
     {
         putchar('\n');
         offset++;
-        for (const auto& [c, node] : children) {
+        for (const auto& [c, node] : *this) {
             for (int i = 0; i < offset; i++)
                 putchar(' ');
             printf("%c", c);
 
-            node->print(offset);
+            node.print(offset);
+        }
+    }
+    void clear()
+    {
+        if (data) {
+            free(data);
+            printf("Deallocation\n");
         }
     }
 };
 
 class Trie {
-    TrieNode root;
+    TrieNode root {};
     void clear(TrieNode& n)
     {
-        for (const auto& [k, v] : n.children) {
-            clear(*v);
-            delete v;
-        }
+        for (const auto& [k, v] : n)
+            clear(v);
+        n.clear();
     }
 
 public:
     ~Trie() { clear(root); };
 
-    void insert(const char* word, Op op)
+    void insert(const char* word)
     {
-        if (op == opUnknown)
-            return;
-
         TrieNode* node = &root;
         while (char c = *word) {
-            auto& foundNode = node->children.insert(c);
-            if (foundNode)
-                node = foundNode;
+            auto& foundNode = node->insert(c);
+            if (foundNode.valid())
+                node = &foundNode;
             else {
-                foundNode = new TrieNode {};
-                node = foundNode;
+                foundNode = TrieNode {};
+                node = &foundNode;
             }
 
             word++;
         }
-        node->op = op;
+        // node->op = op;
     }
 
     void print() { root.print(0); }
 
-    int match(const char* text, int start, Op& op) const
+    int match(const char* text, int start) const
     {
-        const TrieNode* node = &root;
+        TrieNode node = root;
         int len = 0;
 
         for (int i = start; text[i]; ++i) {
-            TrieNode* const* foundValue = node->children.find(text[i]);
-            if (!foundValue)
+            TrieNode foundNode = node.find(text[i]);
+            if (!foundNode.valid())
                 break;
 
-            node = *foundValue;
+            node = foundNode;
             ++len;
-
-            if (node->op == opUnknown) {
-                op = node->op;
-                return len;
-            }
         }
 
         return 0;
@@ -148,11 +203,11 @@ public:
 int main()
 {
     Trie trie;
-    trie.insert("sin", opSin);
-    trie.insert("cos", opCos);
-    trie.insert("-", opMinus);
-    trie.insert("cat", opCat);
-    trie.insert("car", opCat);
+    trie.insert("sin");
+    trie.insert("cos");
+    trie.insert("-");
+    trie.insert("cat");
+    trie.insert("car");
     trie.print();
     return 0;
 }
