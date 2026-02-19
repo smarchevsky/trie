685c5f8a4fce5f04fad0ed822353c7a19562d185
smarchevsky <staseg1987@gmail.com>
2026-02-20 01:16:20 +0200

    Allow duplicates as end word sign

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm> // std::lower_bound
 #include <cassert>
 #include <cstring> // memmove
+#include <flat_set>
 #include <stdint.h>
 #include <stdio.h>
 #include <utility> // std::pair
@@ -13,24 +14,13 @@ inline constexpr size_t align(size_t unaligned)
     static_assert((alignMask & alignof(T)) == 0); // is pow of 2
     return (((size_t)unaligned + alignMask) & ~alignMask);
 }
-
-const char* fruits[] = { "Apple", "Apricot", "Avocado", "Banana", "Bilberry", "Blackberry", "Blackcurrant", "Blueberry",
-    "Boysenberry", "Currant", "Cherry", "Cherimoya", "Chico fruit", "Cloudberry", "Coconut", "Cranberry", "Cucumber", "Custard apple",
-    "Damson", "Date", "Dragonfruit", "Durian", "Elderberry", "Feijoa", "Fig", "Goji berry", "Gooseberry", "Grape", "Raisin",
-    "Grapefruit", "Guava", "Honeyberry", "Huckleberry", "Jabuticaba", "Jackfruit", "Jambul", "Jujube", "Juniper berry", "Kiwano",
-    "Kiwifruit", "Kumquat", "Lemon", "Lime", "Loquat", "Longan", "Lychee", "Mango", "Mangosteen", "Marionberry", "Melon", "Cantaloupe",
-    "Honeydew", "Watermelon", "Miracle fruit", "Mulberry", "Nectarine", "Nance", "Olive", "Orange", "Blood orange", "Clementine",
-    "Mandarine", "Tangerine", "Papaya", "Passionfruit", "Peach", "Pear", "Persimmon", "Physalis", "Plantain", "Plum", "Prune", "Pineapple",
-    "Plumcot", "Pomegranate", "Pomelo", "Purple mangosteen", "Quince", "Raspberry", "Salmonberry", "Rambutan", "Redcurrant", "Salal berry",
-    "Salak", "Satsuma", "Soursop", "Star fruit", "Solanum quitoense", "Strawberry", "Tamarillo", "Tamarind", "Ugli fruit", "Yuzu" };
+#define ARR_SIZE(x) sizeof(x) / sizeof(*x)
 
 template <typename Key, typename Val>
 class BinarySearchMap {
 public:
-    Key* keys = nullptr;
-    Val* vals = nullptr;
-    uint32_t size = 0;
-    uint32_t capacity = 0;
+    std::vector<Key> keys;
+    std::vector<Val> vals;
 
 public:
     struct SoAIterator {
@@ -38,50 +28,35 @@ public:
         const Val* p2;
 
         std::pair<const Key&, const Val&> operator*() const { return { *p1, *p2 }; }
+        bool operator!=(const SoAIterator& other) const { return p1 != other.p1; }
 
         SoAIterator& operator++()
         {
             ++p1, ++p2;
             return *this;
         }
-
-        bool operator!=(const SoAIterator& other) const { return p1 != other.p1; }
     };
 
-    auto begin() const { return SoAIterator { keys, vals }; }
-    auto end() const { return SoAIterator { keys + size, vals + size }; }
+    auto begin() const { return SoAIterator { keys.data(), vals.data() }; }
+    auto end() const { return SoAIterator { keys.data() + keys.size(), vals.data() + vals.size() }; }
 
     Val& insert(const Key& key)
     {
-        auto keyIt = std::lower_bound(keys, keys + size, key);
-        int newKeyPos = std::distance(keys, keyIt);
-
-        if (keyIt == keys + size || *keyIt != key) {
-            size_t _pos = newKeyPos;
-            if (size >= capacity) {
-                size_t _new_cap = (capacity == 0) ? 4 : capacity * 2;
-                keys = (Key*)realloc(keys, _new_cap * sizeof(*keys));
-                vals = (Val*)realloc(vals, _new_cap * sizeof(*vals));
-                capacity = _new_cap;
-            }
-
-            if (_pos < size) {
-                memmove(&keys[_pos + 1], &keys[_pos], (size - _pos) * sizeof(*keys));
-                memmove(&vals[_pos + 1], &vals[_pos], (size - _pos) * sizeof(*vals));
-            }
-
-            keys[_pos] = key;
-            vals[_pos] = Val {};
-            size++;
+        auto keyIt = std::lower_bound(keys.begin(), keys.end(), key);
+        int newKeyPos = std::distance(keys.begin(), keyIt);
+        auto valueIt = vals.begin() + newKeyPos;
+        if (keyIt == keys.end() || *keyIt != key) {
+            keys.insert(keyIt, key);
+            valueIt = vals.emplace(valueIt, Val {});
         }
-        return vals[newKeyPos];
+        return *valueIt;
     }
 
     const Val* find(const Key& key) const
     {
-        auto keyIt = std::lower_bound(keys, keys + size, key);
-        if (keyIt == keys + size || *keyIt != key)
-            return &*(vals + std::distance(keys, keyIt));
+        auto it = std::lower_bound(keys.begin(), keys.end(), key);
+        if (it != keys.end() && *it == key)
+            return &*(vals.begin() + std::distance(keys.begin(), it));
         return nullptr;
     }
 };
@@ -90,6 +65,14 @@ struct TrieNode {
     // Op op = opUnknown;
     bool bStop = false;
     BinarySearchMap<char, TrieNode*> children;
+
+    const auto& getKey(size_t index) const { return children.keys.at(index); }
+    const auto& getNode(size_t index) const { return children.vals.at(index); }
+    size_t getSize() const
+    {
+        assert(children.keys.size() == children.vals.size());
+        return children.keys.size();
+    }
 
     void print(int offset) const
     {
@@ -139,13 +122,13 @@ public:
 
     void print() { root.print(0); }
 
-    int match(const char* text, int start) const
+    int match(const char* text) const
     {
         const TrieNode* node = &root;
         int len = 0;
 
-        for (int i = start; text[i]; ++i) {
-            TrieNode* const* foundValue = node->children.find(text[i]);
+        while (*text) {
+            TrieNode* const* foundValue = node->children.find(*text);
             if (!foundValue)
                 break;
 
@@ -156,6 +139,8 @@ public:
                 // op = node->op;
                 return len;
             }
+
+            text++;
         }
 
         return 0;
@@ -165,7 +150,7 @@ public:
 // for (const auto& [k, v] : node.children)
 using IndexType = uint32_t;
 using NumType = uint8_t;
-using KeyType = uint8_t;
+using KeyType = char;
 
 class DenseTrie {
 public:
@@ -226,58 +211,114 @@ public:
         return 0;
     }
 
-    size_t pack(const TrieNode* node, size_t thisNodeStart)
+    size_t pack(const TrieNode& node, const size_t thisNodeStart)
     {
-        const size_t numStart = thisNodeStart;
-        const size_t keyOffsetStart = numStart + sizeof(NumType);
-        const size_t childNodeOffsetStart = align<IndexType>(keyOffsetStart + node->children.size * sizeof(KeyType));
-        size_t thisNodeEnd = childNodeOffsetStart + node->children.size * sizeof(IndexType);
-
-        m_data.resize(thisNodeEnd);
-
-        *((NumType*)(m_data.data() + numStart)) = node->children.size;
-
-        auto* keys = node->children.keys;
-        auto* nodes = node->children.vals;
-
-        for (int i = 0; i < node->children.size; ++i) {
-            ((KeyType*)(m_data.data() + keyOffsetStart))[i] = keys[i];
-            IndexType* dataAsIndex = (IndexType*)(m_data.data() + childNodeOffsetStart);
-            if (nodes[i]->children.size != 0) {
-                dataAsIndex[i] = thisNodeEnd;
-                thisNodeEnd = pack(nodes[i], thisNodeEnd);
-            } else
-                dataAsIndex[i] = 0;
+        // make additional shift for duplicates
+        int duplicateShift = 0;
+        for (int i = 0; i < node.getSize(); ++i) {
+            const TrieNode* childNode = node.getNode(i);
+            if (childNode->getSize() != 0 && childNode->bStop)
+                duplicateShift++;
         }
 
-        return thisNodeEnd;
+        const size_t nodeSize = node.getSize();
+        const size_t layoutSize = node.getSize() + duplicateShift;
+
+        const size_t numStart = thisNodeStart;
+        const size_t keyStart = numStart + sizeof(NumType);
+        size_t childNodeStart = align<IndexType>(keyStart + layoutSize * sizeof(KeyType));
+        const size_t nodeEndInitial = childNodeStart + layoutSize * sizeof(IndexType);
+        size_t nodeEnd = nodeEndInitial;
+
+        m_data.resize(nodeEnd), assert((size_t)m_data.data() % 8 == 0);
+
+        NumType* numPacked = (NumType*)&m_data.at(numStart);
+        KeyType* packedKeys = (KeyType*)&m_data.at(keyStart);
+        IndexType* packedNodes = (IndexType*)&m_data.at(childNodeStart);
+
+        *numPacked = nodeSize;
+
+        for (int I = 0, packedI = 0; I < nodeSize; ++I, ++packedI) {
+            const auto& childKey = node.getKey(I);
+            const auto& childNode = node.getNode(I);
+
+            packedKeys[packedI] = childKey;
+            packedNodes[packedI] = nodeEnd;
+
+            if (childNode->getSize() != 0) {
+                packedNodes[packedI] = nodeEnd;
+                nodeEnd = pack(*childNode, nodeEnd);
+
+                if (childNode->bStop) {
+                    packedKeys[++packedI] = childKey;
+                    packedNodes[packedI] = 0;
+                    (*numPacked)++;
+                }
+
+            } else {
+                packedNodes[packedI] = 0;
+            }
+        }
+
+        return nodeEnd;
     }
 };
+
+const char* fruits[] = { "apple", "apricot", "avocado", "banana", "bilberry", "blackberry", "blackcurrant", "blueberry",
+    "boysenberry", "currant", "cherry", "cherimoya", "chico fruit", "cloudberry", "coconut", "cranberry", "cucumber", "custard apple",
+    "damson", "date", "dragonfruit", "durian", "elderberry", "feijoa", "fig", "goji berry", "gooseberry", "grape", "raisin",
+    "grapefruit", "guava", "honeyberry", "huckleberry", "jabuticaba", "jackfruit", "jambul", "jujube", "juniper berry", "kiwano",
+    "kiwifruit", "kumquat", "lemon", "lime", "loquat", "longan", "lychee", "mango", "mangosteen", "marionberry", "melon", "cantaloupe",
+    "honeydew", "watermelon", "miracle fruit", "mulberry", "nectarine", "nance", "olive", "orange", "blood orange", "clementine",
+    "mandarine", "tangerine", "papaya", "passionfruit", "peach", "pear", "persimmon", "physalis", "plantain", "plum", "prune", "pineapple",
+    "plumcot", "pomegranate", "pomelo", "purple mangosteen", "quince", "raspberry", "salmonberry", "rambutan", "redcurrant", "salal berry",
+    "salak", "satsuma", "soursop", "star fruit", "solanum quitoense", "strawberry", "tamarillo", "tamarind", "ugli fruit", "yuzu" };
 
 int main()
 {
 
     Trie trie;
+    DenseTrie dtrie;
+#if 0
+    int index = 0;
     for (auto& f : fruits) {
-        // trie.insert(f);
+        printf("Added: %s\n", f);
+        trie.insert(f);
+        if (index++ > 30)
+            break;
     }
+#endif
 
-    trie.insert("sin");
-    trie.insert("cos");
+    trie.insert("ca");
     trie.insert("car");
+    trie.insert("cb");
+    trie.insert("cbz");
 
-    trie.print();
+    // trie.insert("cd");
+    // trie.insert("d");
 
-    DenseTrie dTrie;
-    dTrie.pack(&trie.root, 0);
+    dtrie.pack(trie.root, 0);
 
-    NumType childNum = 0;
-    int len = dTrie.match("car");
-    printf("%d\n", len);
+    // printf("New:  %d\n", dtrie.match("ca"));
+    // printf("New:  %d\n", dtrie.match("car"));
+
+#if 0
+    int numErrors = 0;
+    for (auto& f : fruits) {
+        auto oldLen = trie.match(f);
+        auto newLen = dtrie.match(f);
+        if (oldLen != 0 && newLen == 0) {
+            printf("Old:  %d %s\n", oldLen, f);
+            printf("New:  %d %s\n", newLen, f);
+            numErrors++;
+        }
+    }
+    printf("numErrors: %d\n", numErrors);
+#endif
 
 #if 1
     FILE* f = fopen("tree.bin", "wb");
-    fwrite(dTrie.m_data.data(), 1, dTrie.m_data.size(), f);
+    fwrite(dtrie.m_data.data(), 1, dtrie.m_data.size(), f);
     fclose(f);
 #endif
     return 0;
